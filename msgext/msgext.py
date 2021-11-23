112962f682bd000df53b7a804c9d6e0e6e62ae56
Avery Munoz <munoza@wit.edu>
2021-11-23 12:54:17 -0500

    adds arg handling to msgext

--- msgext/msgext.py
+++ msgext/msgext.py
@@ -4,8 +4,7 @@ import rosbag
 import sys
 import argparse
 import os
-
-base_path = 'workspace' #base folder location, defaults to 'workspace' in working directory 
+import glob
 
 def mkdir(string):
     direct = base_path
@@ -23,50 +22,79 @@ def mkdir(string):
 
 
 def main():
-
-    try:
-        os.mkdir(base_path)
-    except FileExistsError:
-        print('INFO: Workspace already exists')
-     
+    global base_path
+    base_path = 'workspace' #base folder location, defaults to 'workspace' in working directory
     types = {}
 
-    for bagname in sys.argv[1:]:
+    parser = argparse.ArgumentParser(description='Read, analyze and build temp workspaces containing .msg definitions from a ROS .bag file,.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('bag', 
+                        nargs='+', 
+                        help='Path of a bag file or a folder of bag files.')
+    parser.add_argument("-v", help="Verbose mode, prints msg definitions as they're read", 
+                        action="store_true")
+    parser.add_argument("-w", help="Workspace mode, automatically generates a ROS workspace containing msg definitions", 
+                        action="store_true")
+    parser.add_argument("-n", help="Name of workspace folder, default is 'workspace'")
+    args = parser.parse_args()
+
+    full_paths = [os.path.join(os.getcwd(), path) for path in args.bag]
+    files = set()
+    for path in full_paths:
+        if os.path.isfile(path):
+            files.add(path)
+            print('[INFO] Opening bag: ' + path + '\n')
+        else:
+            files |= set(glob.glob(path + '/*' + args.extension))
+
+    # print(args.bag)
+    
+    if args.n:
+        base_path = args.n 
+
+    for bagname in files:
+        
         bag = rosbag.Bag(bagname)
         for topic, msg, t in bag.read_messages():
             types[msg._type] = msg._full_text 
 
+    if args.w:
+        try:
+            os.mkdir(base_path)
+        except FileExistsError:
+            print()
+
     for t in types:
 
-        # Print msg def to std out first #
-        print ("Message type:", t)
-        print ("Message text:")
-        print (types[t])
-        print ()
-        ##################################
-        
-        direct = mkdir(t)
-        f = open(direct + '.msg', 'w+')
-        lines = types[t].split('MSG: ',-1)
-        first = True
-        for x in lines:
-            if first:
-                f.write(x)
-                first = False
-                f.close()
-            else:
-                sublines = x.split('\n',-1)
-                path = sublines[0].strip('MSG: ')
-                
-                subtype = mkdir(path)
-                f = open(subtype + '.msg', 'w+')
-                first2 = True
-                for i in sublines:
-                    if(first2):
-                        first2 = False
-                    else:
-                         f.write(i + '\n')
-                f.close()
+        # Print msg def to std out #
+        if args.v:
+            print ("Message type:", t)
+            print ("Message text:")
+            print (types[t])
+            print ()
+        ############################
+        if args.w:
+            direct = mkdir(t)
+            f = open(direct + '.msg', 'w+')
+            lines = types[t].split('MSG: ',-1)
+            first = True
+            for x in lines:
+                if first:
+                    f.write(x)
+                    first = False
+                    f.close()
+                else:
+                    sublines = x.split('\n',-1)
+                    path = sublines[0].strip('MSG: ')
+                    
+                    subtype = mkdir(path)
+                    f = open(subtype + '.msg', 'w+')
+                    first2 = True
+                    for i in sublines:
+                        if(first2):
+                            first2 = False
+                        else:
+                            f.write(i + '\n')
+                    f.close()
       
       
 
